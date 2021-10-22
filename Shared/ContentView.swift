2c33ff7e6a5531559ad5fca4e1404810389145d8
KV Ghumaan <kv.s.ghumaan@gmail.com>
2021-10-21 23:14:37 -0700

    need to update prgress colors..

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -15,11 +15,10 @@ struct ContentView: View {
         HStack {
             VStack {
                 if !wagmiAvailable {
-                    Button("ADOPT NEW HOMEBOI!", action: {
+                    Button("ADOPT NEW HOMEBOI!"){
                         self.wagmiAvailable.toggle()
-                    })
+                    }
                         .foregroundColor(.white)
-                        .padding()
                         .background(Color.accentColor)
                         .cornerRadius(8)
                 }else{
@@ -37,7 +36,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct DisplayHomeboi: View {
     
-    @State var mapViewEnabled = true
+    @State var mapViewEnabled = false
     
     var body: some View {
         
@@ -46,36 +45,48 @@ struct DisplayHomeboi: View {
             MapTravelView()
             
         }else{
-            Text("Gang") // Name of the homeboi
             
-            HomeboiGif("384")  // Name of GIF
             
-            List {
-                HStack {
-                    Text("Health:")
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.red)
-                        .scaleEffect(CGSize(width: 1, height: 0.1))
-                }
-                HStack {
-                    Text("Happiness:")
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.yellow)
-                        .scaleEffect(CGSize(width: 1, height: 0.1))
-                }
-                HStack {
-                    Text("Steps accumulated:")
-                    Text("2,324").font(.headline).bold().italic()
-                }
+            VStack {
                 
-                Button(action: {
-                    self.mapViewEnabled.toggle()
-                }){
-                    Text("Map View")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .border(Color.blue)
-                        .padding()
+                HomeboiGif("384").frame(width: .infinity, height: 260, alignment: .center)  // TODO: height should reflect size of GIF
+                
+                List {
+                    HStack {
+                        Text("Health:").frame(width: 90, height: 0.2, alignment: Alignment.leading)
+                        ProgressView(value: 0.6)
+                    }
+                    HStack {
+                        Text("Happiness:").frame(width: 90, height: 0.2, alignment: Alignment.leading)
+                        ProgressView(value: 0.8)
+                    }
+                    HStack {
+                        Text("Charm:").frame(width: 90, height: 0.2, alignment: Alignment.leading)
+                        ProgressView(value: 0.4)
+                    }
+                    HStack {
+                        Text("Rest:").frame(width: 90, height: 0.2, alignment: Alignment.leading)
+                        ProgressView(value: 1.0)
+                    }
+                    HStack {
+                        Text("Steps accumulated:")
+                        Text("2,324").font(.headline).bold().italic().frame(alignment: Alignment.trailing)
+                    }
+                    VStack {
+                        Text("NEXT LEVEL:").bold().frame(maxWidth: .infinity, alignment: .leading)
+                        HStack {
+                            ProgressView(value: 0.8).colorMultiply(Color.red)
+                            Image(systemName: "star.fill").foregroundColor(Color.yellow)
+                        }
+                    }
+                    
+                    Button(action: {
+                        self.mapViewEnabled.toggle()
+                    }){
+                        Text("Map View")
+                            .frame(maxWidth: .infinity)
+                            .border(Color.blue)
+                    }
                 }
             }
         }
@@ -84,11 +95,21 @@ struct DisplayHomeboi: View {
 }
 
 struct MapTravelView: View{
-    
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     
     var body: some View {
         Map(coordinateRegion: $region)
             .scaleEffect(CGSize(width: 1, height: 1)).preferredColorScheme(ColorScheme.dark)
+    }
+}
+
+struct ProgressBar: ProgressViewStyle {
+    let color: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+        
+        // TODO: figure out how to set color...
+        ProgressView(configuration).shadow(color: Color(red: 0.3, green: 0.8, blue: 0.6),
+                    radius: 4.0, x: 1.0, y: 2.0)
     }
 }
