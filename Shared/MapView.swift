3a84b742f955ca2edd8d7bf21b7179f89faf52cb
KV Ghumaan <kv.s.ghumaan@gmail.com>
2021-10-25 20:35:28 -0700

    checking if location services are enabled

--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -12,6 +12,8 @@ struct TravelView: View{
     
     @Binding var mapViewEnabled: Bool
     
+    @StateObject private var viewModel = TravelViewModel()
+    
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     
     var body: some View {
@@ -20,10 +22,30 @@ struct TravelView: View{
         }) {
             Text("dismiss")
         }
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region, showsUserLocation: true)
             .scaleEffect(CGSize(width: 1, height: 1))
             .preferredColorScheme(ColorScheme.dark)
             .ignoresSafeArea()
         
+    }
+}
+
+final class TravelViewModel: ObservableObject {
+    var locationManager: CLLocationManager?
+    
+    func isLocationServicesEnabled(){
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager = CLLocationManager()
+            locationManager?.activityType = .fitness
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        }else{
+            print("location services disabled")
+//            Alert(title: Text("Location Services Disabled"),
+//                  message: Text("Please enable to use all of our features"),
+//                  primaryButton: Alert.Button.default(Text("Enable"), action: {
+//                print("TODO: direct user to enable user services")
+//            }),
+//                  secondaryButton: Alert.Button.cancel())
+        }
     }
 }
