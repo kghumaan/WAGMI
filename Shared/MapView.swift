ede4b64fc4ab238c6f79f2f28b48859b231f99c4
KV Ghumaan <kv.s.ghumaan@gmail.com>
2021-10-25 21:04:11 -0700

    map view location prompt/validation

--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -14,28 +14,33 @@ struct TravelView: View{
     
     @StateObject private var viewModel = TravelViewModel()
     
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-    
     var body: some View {
         Button(action: {
             self.mapViewEnabled.toggle()
         }) {
             Text("dismiss")
         }
-        Map(coordinateRegion: $region, showsUserLocation: true)
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
             .scaleEffect(CGSize(width: 1, height: 1))
             .preferredColorScheme(ColorScheme.dark)
+            .accentColor(Color.pink)
             .ignoresSafeArea()
+            .onAppear{
+                viewModel.ensureLocationServicesEnabled()
+            }
         
     }
 }
 
-final class TravelViewModel: ObservableObject {
+final class TravelViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     var locationManager: CLLocationManager?
     
-    func isLocationServicesEnabled(){
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    
+    func ensureLocationServicesEnabled(){
         if CLLocationManager.locationServicesEnabled(){
             locationManager = CLLocationManager()
+            locationManager!.delegate = self
             locationManager?.activityType = .fitness
             locationManager?.desiredAccuracy = kCLLocationAccuracyBest
         }else{
@@ -47,5 +52,33 @@ final class TravelViewModel: ObservableObject {
 //            }),
 //                  secondaryButton: Alert.Button.cancel())
         }
+    }
+    
+    private func ensureLocationIsAuthorized(){
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus{
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            // TODO: test this scenario
+            _ = Alert(title: Text("Location Restricted"),
+                      message: Text("It's likely you have parental controls enabled. Please unrestrict location prior to continuing."))
+        case .denied:
+            // TODO: test this scenario
+            _ = Alert(title: Text("Location Access Needed"),
+                      message: Text("You miss out on most of our features without location services enabled. Please toggle in settings."))
+        case .authorizedAlways, .authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate,
+                                        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        @unknown default:
+            break
+        }
+
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        ensureLocationIsAuthorized()
     }
 }
